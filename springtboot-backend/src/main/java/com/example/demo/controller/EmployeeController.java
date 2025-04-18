10aa2c38dfdd7c4a91b07dc18420e318398dfd99
Enigma-X94 <souidi.nassim94@gmail.com>
2025-04-18 17:57:34 +0100

    Update EmployeeController.java

--- springtboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
+++ springtboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
